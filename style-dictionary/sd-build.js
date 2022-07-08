548bb11578adaccd3deb1cc6de28476958e20362
Juan Pablo Michelli <juan.michelli@chamberlain.com>
2022-07-08 15:25:49 -0300

    Added node modules for SD and created my first transform and transform group

---
+++ style-dictionary/sd-build.js
@@ -0,0 +1,18 @@
+const StyleDictionary = require('style-dictionary').extend('./style-dictionary/sd-config.json');
+
+StyleDictionary.registerTransform({
+    name: 'radii/px',
+    type: 'value',
+    matcher: function(token){
+        return token.attributes.category === 'radii' && token.value != 0;
+    },
+    transformer: function(token){
+        return token.value + 'px';
+    }
+})
+
+StyleDictionary.registerTransformGroup({
+    name: "less",
+    transforms: StyleDictionary.transformGroup["less"].concat(['radii/px'])
+})
+StyleDictionary.buildAllPlatforms();
